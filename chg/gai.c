0b5aef9cb7b50099488f4558b5622c7923bf8542
artembunichev <artembunichev@zohomail.com>
2024-09-02 22:05:03 +0300

    add changes for gallons and pints.

---
+++ chg/gai.c
@@ -0,0 +1,6 @@
+/*change imperial (United Kingdom) gallons into litres.*/
+#include<stdio.h>
+int main(){
+float x;
+while(1){scanf("%f",&x);dprintf(1,"%f\n",x*4.54609);}
+return 0;}
---
+++ chg/ptu.c
@@ -0,0 +1,6 @@
+/*change US pints into litres.*/
+#include<stdio.h>
+int main(){
+float x;
+while(1){scanf("%f",&x);dprintf(1,"%f\n",x*0.473176473);}
+return 0;}
